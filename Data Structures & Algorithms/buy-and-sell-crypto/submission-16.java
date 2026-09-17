b9a149ab97ab7a11fd93725b4e455936e5d8ce87
mohitsa369 <sahnimohit369@gmail.com>
2026-09-17 13:04:28 +0530

    Add: buy-and-sell-crypto - submission-16

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-16.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int n= prices.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(prices[j] > prices[i]){
+                    int profit = prices[j] - prices[i];
+                    maxProfit = Math.max(maxProfit, profit);
+                }
+            }
+        }
+        return maxProfit;
+        
+    }
+}
